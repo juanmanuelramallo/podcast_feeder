a7551468ee9cc772cade663a4511c108a4293d70
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2020-06-24 01:39:35 -0300

    Adding basic auth (#7)
    * Adding basic auth
    
    * Adding basic auth env vars to ci

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    return unless request.format != Mime[:rss]
+
+    http_basic_authenticate_or_request_with(
+      name: ENV.fetch('HTTP_BASIC_AUTH_NAME'),
+      password: ENV.fetch('HTTP_BASIC_AUTH_PASSWORD')
+    )
+  end
 end
