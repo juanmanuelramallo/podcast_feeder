e1e44cd81dbc9bfa340fc51aab8346318d5ccdc0
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2020-09-20 16:54:55 -0300

    Attempt to fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rails'
 require 'selenium/webdriver'
+require 'rspec-log_matcher'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
