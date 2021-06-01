7ef505d558413262100b62d6f5d25485ddc2b3eb
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2021-06-01 16:36:15 -0300

    Fix slug path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :episodes, except: [:index]
   end
 
-  get '/*slug', to: 'landing#show'
+  get '/*slug', to: 'landing#show', constraints: ->(req) { !req.path.starts_with?('/rails') }
 
   root 'landing#show'
 end
