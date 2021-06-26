561a2b32ad29f8bbd9e8ba9282d218ed880dcea0
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2021-06-26 18:32:17 -0300

    Rubocop fix

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank? && options[:allow_blank]
-    return if value.match?(URI::regexp)
+    return if value.match?(URI::DEFAULT_PARSER.make_regexp)
 
     record.errors.add(attribute, 'is not a valid URL')
   end
--- db/migrate/20210602163833_add_podcast_providers_to_podcasts.rb
+++ db/migrate/20210602163833_add_podcast_providers_to_podcasts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPodcastProvidersToPodcasts < ActiveRecord::Migration[6.0]
   def change
     change_table :podcasts do |t|
